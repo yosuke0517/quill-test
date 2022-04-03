67f8dba14a5173455a4f2e87530b6ae2165fa180
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2022-04-03 19:16:58 +0900

    init

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+import { QuillEditor } from "@vueup/vue-quill";
+// import "@vueup/vue-quill/dist/vue-quill.snow.css";
+import "@vueup/vue-quill/dist/vue-quill.bubble.css";
+
+const addEditor = () => {
+  console.log("addEditor");
+};
+</script>
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <QuillEditor theme="bubble" toolbar="essential" />
+    <button type="button" @click="addEditor">add Editor</button>
   </div>
 </template>
