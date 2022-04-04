112cd1f8d48d1e55e2748b50540a904e92e607bd
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2022-04-05 00:29:36 +0900

    add コンポーネントを動的に追加する機能の作成

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,26 @@
-import { createStore } from "vuex";
+import { InjectionKey } from "vue";
+import { createStore, Store, useStore as baseUseStore } from "vuex";
+import * as MutationTypes from "./mutationTypes";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+// stateの型定義
+type State = { components: unknown[] };
+
+// storeをprovide/injectするためのキー
+export const key: InjectionKey<Store<State>> = Symbol();
+
+// store本体
+export const store = createStore<State>({
+  state: {
+    components: [],
+  },
+  mutations: {
+    [MutationTypes.ADD_COMPONENT](state, component: unknown) {
+      state.components.push(component);
+    },
+  },
 });
+
+// useStoreを使う時にキーの指定を省略するためのラッパー関数
+export const useStore = () => {
+  return baseUseStore(key);
+};
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,73 @@
 <script setup lang="ts">
+import { computed } from "@vue/reactivity";
 import { QuillEditor } from "@vueup/vue-quill";
-// import "@vueup/vue-quill/dist/vue-quill.snow.css";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
 import "@vueup/vue-quill/dist/vue-quill.bubble.css";
+import { onMounted, ref } from "vue";
+import { useStore } from "@/store";
+import { ADD_COMPONENT } from "@/store/mutationTypes";
 
+const store = useStore();
+const components = computed(() => store.state.components);
+const addComponent = (component: unknown) => {
+  store.commit(ADD_COMPONENT, {
+    components: component,
+  });
+};
 const addEditor = () => {
   console.log("addEditor");
 };
+
+const addOther = () => {
+  console.log("addEditor");
+};
+
+const handleClick = () => {
+  console.log("hoge");
+  console.log(myQuillEditor.value);
+};
+
+const myQuillEditor = ref<HTMLElement>();
+
+onMounted(() => {
+  console.log(myQuillEditor.value);
+});
 </script>
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <QuillEditor theme="bubble" toolbar="essential" />
-    <button type="button" @click="addEditor">add Editor</button>
+    <QuillEditor
+      ref="myQuillEditor"
+      theme="bubble"
+      toolbar="essential"
+      placeholder="入力してください"
+      :content="'<h2>I am Example</h2>'"
+      @click="handleClick"
+    />
+    <!-- <h1>This is an about page</h1>
+    <QuillEditor
+      theme="snow"
+      toolbar="essential"
+      placeholder="入力してください"
+    /> -->
+    <QuillEditor
+      theme="bubble"
+      toolbar="essential"
+      placeholder="入力してください"
+      v-for="(component, i) in components"
+      :key="i"
+    />
+    <div class="ButtonGroup">
+      <button type="button" @click="addComponent('QuillEditor')">
+        add Editor
+      </button>
+      <button type="button" @click="addOther">add Other</button>
+    </div>
   </div>
 </template>
+<style lang="scss" scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+}
+</style>
